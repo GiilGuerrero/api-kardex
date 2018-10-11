2ec89376fda9785e8c69304c0b28a7e9f22df9ee
Gilberto Guerrero <giil.guerrero@hotmail.com>
2018-10-11 09:50:03 -0500

    Kardex

--- servidor.js
+++ servidor.js
@@ -6,22 +6,43 @@ var app = express();
 app.use(express.son());
 app.get('/calificaciones', function(req,res){
     console.log('Token recibido' + req.query.token);
+    jwt,verify(req.query.token, 'claveSecreta', 
+    function(error){
+        if(error){
+            res.status(403).json({mensaje: 'Autorización no valida'});
+        }
+        else{
+            res.json({
+                mensaje: 'Aquí están las calificaciones'
+            });
+        }    
+    });
     res.json({
         mensaje: 'Aquí estan las calificaciones'
     });
 });
 
 app.post('/login', function(req,res){
-    var token = jwt.sign({
-        usuario: 'alumno',
-        nombre: 'Gilberto',
-        claveUnica: 227505
+    
+    var alumno = {
+        email: 'alumno@uaslp.mx',
+        password: '123'
+    };
 
-    }, 'claveSecreta', {expiresIn: '60s'});
-    console.log('Token generado: ' + token);
-    res.json({
-        Token: token
-    });
+    if(req.body.email == alumno.email && req.body.password == alumno.password)
+    {
+        var token = jwt.sign({
+            usuario: 'alumno',
+            nombre: 'Gilberto',
+            claveUnica: 227505
+    
+        }, 'claveSecreta', {expiresIn: '1h'});
+        console.log('Token generado: ' + token);
+        res.json({
+            Token: token
+        });
+    }
+
 });
 
 app.listen(puerto,function(){
